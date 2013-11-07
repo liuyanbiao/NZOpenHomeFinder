a14f9f0fd85f7258bac25fe6871491854bc08f82
bill.nz <bill.nz@gmail.com>
2013-11-07 16:19:58 +1300

    first commit

---
+++ gen/com/example/nzopenhomefinder/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.nzopenhomefinder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/nzopenhomefinder/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.nzopenhomefinder;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	final Context context = this;
+
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+	}
+
+	private class LoadWebPageASYNC extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... urls) {
+
+			WebView webView = (WebView) findViewById(R.id.webView);
+			webView.getSettings().setJavaScriptEnabled(true);
+			webView.loadUrl(urls[0]);
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+
+		}
+	}
+
+	public void dummyFunc(View view){
+		Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+
+	}
+
+	public void readWebpage(View view) {
+		LoadWebPageASYNC task = new LoadWebPageASYNC();
+		task.execute(new String[] { "http://www.skykiwi.com" });
+
+	}
+}
