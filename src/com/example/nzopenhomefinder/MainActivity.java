d52c014f13f29ed608e4f2adf0d4f951ae48515e
liuyanbiao <bill.nz@gmail.com>
2013-12-05 18:28:47 +1300

    again

--- src/com/example/nzopenhomefinder/MainActivity.java
+++ src/com/example/nzopenhomefinder/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
 	
 	
 	
-//try again on Dec 5th
+//try again on Dec 5th again againaing 
 	
 	private class LoadWebPageASYNC extends AsyncTask<String, Void, String> {
 
